54f5b61a9af83338107bd7db1e404bab033afbfb
samroodAli <devsamrood@gmail.com>
2021-04-11 18:05:02 +0530

    join tutorial and quiz answers from sqlzoo

--- answers.sql
+++ answers.sql
@@ -344,9 +344,100 @@ No result due to invalid use of the WHERE function
 
  SELECT name, population/area AS density FROM bbc WHERE population = (SELECT MAX(population) FROM bbc)
 
-
 Table-D
 Americas	732240
 Middle East	13403102
 South America	17740392
 South Asia	9437710
+
+SELECT matchid,player FROM goal 
+  WHERE teamid='GER'
+
+SELECT DISTINCT id,stadium,team1,team2
+  FROM game
+WHERE id= 1012
+
+SELECT player,teamid,stadium,mdate
+  FROM game JOIN goal ON (id=matchid)
+WHERE teamid ='GER'
+
+SELECT team1,team2,player
+  FROM game JOIN goal ON (id=matchid)
+WHERE player LIKE 'Mario%'
+
+SELECT player, teamid, coach,gtime
+  FROM goal JOIN eteam ON teamid=id
+ WHERE gtime<=10
+
+SELECT mdate,teamname
+FROM game
+JOIN eteam
+ON team1 = eteam.id
+WHERE coach = 'Fernando Santos'
+
+SELECT player
+FROM game 
+JOIN goal ON (game.id = goal.matchid)
+WHERE stadium = 'National Stadium, Warsaw'
+
+SELECT DISTINCT player
+FROM game JOIN goal ON matchid = id 
+WHERE (team1='GER' OR team2='GER')
+AND teamid <> 'GER'
+
+SELECT teamname, COUNT(player)
+ FROM eteam JOIN goal ON id=teamid GROUP BY teamname
+
+SELECT stadium ,COUNT(player)
+  FROM game
+  JOIN goal ON(id=matchid)
+GROUP BY stadium
+
+SELECT matchid,mdate, COUNT(mdate)
+  FROM game JOIN goal ON matchid = id 
+ WHERE (team1 = 'POL' OR team2 = 'POL') AND id = matchid GROUP BY mdate,matchid
+
+SELECT matchid,mdate, COUNT(mdate)
+  FROM game JOIN goal ON matchid = id 
+ WHERE teamid = 'GER' GROUP BY matchid,mdate
+
+SELECT mdate,
+  team1,
+  SUM(CASE WHEN teamid=team1 THEN 1 ELSE 0 END) AS score1,
+team2,
+  SUM(CASE WHEN teamid=team2 THEN 1 ELSE 0 END) AS score2
+  FROM game JOIN goal ON matchid = id GROUP BY mdate,matchid,team1,team2
+ORDER BY mdate,matchid,team1,team2
+
+
+#join quiz
+
+ game  JOIN goal ON (id=matchid)
+
+matchid, teamid, player, gtime, id, teamname, coach
+
+SELECT player, teamid, COUNT(*)
+  FROM game JOIN goal ON matchid = id
+ WHERE (team1 = "GRE" OR team2 = "GRE")
+   AND teamid != 'GRE'
+ GROUP BY player, teamid
+
+DEN	9 June 2012
+GER	9 June 2012
+
+SELECT DISTINCT player, teamid 
+FROM game JOIN goal ON matchid = id 
+WHERE stadium = 'National Stadium, Warsaw' 
+AND (team1 = 'POL' OR team2 = 'POL')
+AND teamid != 'POL'
+
+SELECT DISTINCT player, teamid, gtime
+FROM game JOIN goal ON matchid = id
+WHERE stadium = 'Stadion Miejski (Wroclaw)'
+AND (( teamid = team2 AND team1 != 'ITA') OR ( teamid = team1 AND team2 != 'ITA'))
+
+
+Netherlands	2
+Poland	2
+Republic of Ireland	1
+Ukraine	2
